78a6c2b6785091a79488753c70dd39c09db000fa
Владислав <pahnetstarushkami@bk.ru>
2024-05-27 19:13:48 +0300

    fix maybeeee

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+const MarkdownEditor = dynamic(
+  () => import("@uiw/react-markdown-editor").then((mod) => mod.default),
+  { ssr: false }
+);
+
 export default nextConfig;
