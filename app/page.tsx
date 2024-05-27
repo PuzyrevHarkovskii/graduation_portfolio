a8e219b68dc3a3bdb8512edd1c0bb020cdea0a64
Владислав <pahnetstarushkami@bk.ru>
2024-05-27 19:22:48 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,9 @@ import Image from "next/image";
 import RevealBento from "@/components/Courses.jsx";
 
 export default function Home() {
-  return <>{/* <RevealBento /> */}</>;
+  return (
+    <>
+      <RevealBento />
+    </>
+  );
 }
